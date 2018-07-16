dbb8ce4571d31a0eb90ca3e6b07d0f1e4702f73a
Chmouel Boudjnah <chmouel@chmouel.com>
2018-07-16 20:37:05 +0200

    vars/osio.groovy

--- vars/osio.groovy
+++ vars/osio.groovy
@@ -48,7 +48,7 @@ def call(Map parameters = [:], body) {
 
     sleep(time: 10, unit: "MINUTES")
 
-    sh "oc get project -o go-template='{{range .items}}{{.metadata.name}}{{\"\n\"}}{{end}}'|sed -n '/-jenkins/{ s/-jenkins//;p;}'""
+    sh "oc get project -o go-template='{{range .items}}{{.metadata.name}}{{\"\\n\"}}{{end}}'|sed -n '/-jenkins/{ s/-jenkins//;p;}'""
 
     print "DEBUG: parameter foo = ${config}"
 
